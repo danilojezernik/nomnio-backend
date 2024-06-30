f20ac8bd4632fabac111733479b2344dd51198e6
danilojezernik <dani.jezernik@gmail.com>
2024-06-30 13:53:15 +0200

    1. Changes to route for weather-forecast

--- src/env.py
+++ src/env.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 PORT = int(os.getenv('PORT'))
 API_KEY = os.getenv('API_KEY')
-LAT = float(os.getenv('LAT'))
-LON = float(os.getenv('LON'))
+LAT = int(os.getenv('LAT'))
+LON = int(os.getenv('LON'))
 UNIT = os.getenv('UNIT')
